cdd5b1d683912ab95cc51c91c4454ee912ec0891
Maarten de Boer <maarten@cloudstek.nl>
2017-03-07 02:09:05 +0100

    Fix bugs, improve fetch, update checking and much more

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
 const ava = require('gulp-ava');
 
+/**
+ * Transpile using babel
+ */
 const transpileTask = () => {
     let babelrc = JSON.parse(fs.readFileSync('.babelrc'));
 
-    return gulp.src('src/*.js.flow')
-        .pipe(gulp.dest('.'))
+    return gulp.src('*.js.flow')
         .pipe(babel(babelrc))
         .pipe(rename({
             extname: ''
@@ -17,21 +19,28 @@ const transpileTask = () => {
         .pipe(gulp.dest('.'));
 };
 
+/**
+ * Lint using eslint (and babel)
+ */
 const lintTask = () => {
-    return gulp.src('src/*.js.flow')
+    return gulp.src('*.js.flow')
         .pipe(eslint())
         .pipe(eslint.format('node_modules/eslint-formatter-pretty'))
         .pipe(eslint.failAfterError());
 };
 
+/**
+ * Run tests using ava
+ */
+const testTask = () => {
+    return gulp.src('test/*.js')
+        .pipe(ava({verbose: true}));
+};
+
 gulp.task('lint', lintTask);
 gulp.task('transpile', transpileTask);
 gulp.task('build', ['lint'], transpileTask);
-
-gulp.task('test', ['build'], () => {
-    return gulp.src('test/*.js')
-        .pipe(ava({verbose: true}));
-});
+gulp.task('test', ['build'], testTask);
 
 gulp.task('watch', ['build'], done => {
     gulp.watch('src/*.js.flow', ['transpile']);
